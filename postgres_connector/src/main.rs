e99848fd90dc65f9ddc24f7761db56f684c0a462
Barsik-sus <bogdan.balushkin5@gmail.com>
2023-03-14 15:25:55 +0200

    Postgress 1000

--- postgres_connector/src/main.rs
+++ postgres_connector/src/main.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut client = Client::connect(&std::env::var("DATABASE_URL")?, NoTls)?;
 
-    for i in 0..1_00 {
+    for i in 0..1_000 {
         let _result = client.query(&format!("SELECT * FROM table_{i}"), &[])?;
     }
 
